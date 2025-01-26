95dcb1f35fd9ee101a920699063fa1b648ee3ac5
Diogo Mota <xdimota7x@gmail.com>
2025-01-26 19:45:09 +0000

    Add initial env config

---
+++ pwa-blog/backend/src/config/env.ts
@@ -0,0 +1,20 @@
+/** Application config env vars */
+export const config = {
+  port: process.env.PWA_BLOG_BACKEND__PORT || 3000,
+  logLEvel: process.env.PWA_BLOG_BACKEND__LOG_LEVEL || 'verbose',
+  isProduction: process.env.NODE_ENV === 'production',
+  mongodbConnectionURL:
+    process.env.PWA_BACKEND__MONGODB_CONNECTION_URL ||
+    'mongodb://localhost:27017/pwa-blog',
+  auth: {
+    jwtSecret: process.env.PWA_BACKEND__JWT_SECRET || 'random-jwt-secret',
+    jwtAccessDurationSeconds:
+      Number(process.env.PWA_BACKEND__JWT_ACCESS_DURATION_SECONDS) ||
+      1 * 60 * 60, // 1 hour
+    jwtRefreshDurationSeconds:
+      Number(process.env.PWA_BACKEND__JWT_DURATION_DURATION_SECONDS) ||
+      10 * 60 * 60 * 24, // 10 days
+    passwordSaltOrRounds:
+      Number(process.env.PWA_BACKEND__PASSWORD_SALT_ROUNDS) || 10,
+  },
+} as const;
