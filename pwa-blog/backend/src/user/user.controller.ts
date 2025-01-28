b7c379157db2b6df739a9e52d49749dbe8af28b7
Diogo Mota <xdimota7x@gmail.com>
2025-01-28 19:16:06 +0000

    Add user controller and module

---
+++ pwa-blog/backend/src/user/user.controller.ts
@@ -0,0 +1,84 @@
+import {
+  Get,
+  Put,
+  Post,
+  Body,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Controller,
+} from '@nestjs/common';
+import { ZodSerializerDto } from 'nestjs-zod';
+import { Role } from '@pwa/shared';
+import { UserService } from './user.service';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { UserDocument } from './schemas/user.schema';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { UserDto } from './dto/user.dto';
+import {
+  VerifyAvailableEmailRequestDto,
+  VerifyAvailableEmailResponseDto,
+} from './dto/verify-available-email.dto';
+import { IdDto } from '../shared/dto/id.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('me')
+  @Auth()
+  @ZodSerializerDto(UserDto)
+  getMyUserInfo(@CurrentUser() user: UserDocument) {
+    return user;
+  }
+
+  @Post('verify-available-email')
+  @HttpCode(HttpStatus.OK)
+  @ZodSerializerDto(VerifyAvailableEmailResponseDto)
+  async verifyAvailableEmail(
+    @Body() verifyAvailableEmailDto: VerifyAvailableEmailRequestDto,
+  ): Promise<VerifyAvailableEmailResponseDto> {
+    const isAvailable = await this.userService.verifyAvailableEmail(
+      verifyAvailableEmailDto,
+    );
+    return { isAvailable };
+  }
+
+  @Get()
+  @Auth(Role.Admin)
+  @ZodSerializerDto(UserDto)
+  getAll() {
+    return this.userService.getAll();
+  }
+
+  @Get(':id')
+  @Auth(Role.Admin)
+  @ZodSerializerDto(UserDto)
+  async getById(@Param() params: IdDto) {
+    return this.userService.getById(params.id);
+  }
+
+  @Post()
+  @Auth(Role.Admin)
+  @ZodSerializerDto(UserDto)
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Put(':id')
+  @Auth(Role.Admin)
+  @ZodSerializerDto(UserDto)
+  async update(@Param() params: IdDto, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(params.id, updateUserDto);
+  }
+
+  @Delete(':id')
+  @Auth(Role.Admin)
+  async delete(@Param() params: IdDto) {
+    await this.userService.delete(params.id);
+    return { success: true };
+  }
+}
---
+++ pwa-blog/backend/src/user/user.module.ts
@@ -0,0 +1,17 @@
+import { Global, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { User, UserSchema } from './schemas/user.schema';
+import { UserSeeder } from './seed/user.seeder';
+
+@Global()
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UserController],
+  providers: [UserService, UserSeeder],
+  exports: [UserService],
+})
+export class UserModule {}
