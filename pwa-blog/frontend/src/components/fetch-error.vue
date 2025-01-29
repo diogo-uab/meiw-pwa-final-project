2bfe43a8e8458fb80a06139aecaa15b5e092c804
Diogo Mota <xdimota7x@gmail.com>
2025-01-29 21:28:06 +0000

    Add fetch-error component

---
+++ pwa-blog/frontend/src/components/fetch-error.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+import { twMerge } from '../utils/tailwind';
+
+type FetchErrorProps = {
+  label: string,
+  labelClass?: string,
+  iconClass?: string,
+};
+
+defineProps<FetchErrorProps>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-4 items-center" v-tw-merge>
+    <Icon
+      icon="mdi-error"
+      :class="twMerge('text-red-500 text-8xl', iconClass)"
+    />
+    <p :class="twMerge('text-xl font-semibold', labelClass)">
+      {{ label }}
+    </p>
+  </div>
+</template>
