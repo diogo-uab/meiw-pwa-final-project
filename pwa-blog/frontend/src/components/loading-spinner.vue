a68efc22abd8bfeb23db522ba8e01986a8b525d3
Diogo Mota <xdimota7x@gmail.com>
2025-01-29 21:29:18 +0000

    Add loading-spinner component

---
+++ pwa-blog/frontend/src/components/loading-spinner.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+import { twMerge } from '../utils/tailwind';
+
+type LoadingSpinnerProps = {
+  icon?: string,
+  size?: 'small' | 'normal' | 'large' | 'huge',
+  variant?: 'primary' | 'secondary',
+};
+
+const {
+  icon = 'line-md:loading-twotone-loop',
+  size = 'normal',
+  variant = 'primary',
+} = defineProps<LoadingSpinnerProps>();
+</script>
+
+<template>
+  <Icon
+    v-tw-merge
+    :icon="icon"
+    :class="twMerge(
+      'text-primary text-2xl',
+      size === 'small' && 'text-base',
+      size === 'large' && 'text-6xl',
+      size === 'huge' && 'text-9xl',
+      variant === 'secondary' && 'text-white',
+    )"
+  />
+</template>
