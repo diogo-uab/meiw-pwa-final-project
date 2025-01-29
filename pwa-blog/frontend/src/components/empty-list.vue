4ccb2c049136633ac287e33cd25ec52a521d6b20
Diogo Mota <xdimota7x@gmail.com>
2025-01-29 21:27:28 +0000

    Add empty-list component

---
+++ pwa-blog/frontend/src/components/empty-list.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+import { twMerge } from '../utils/tailwind';
+
+type EmptyListProps = {
+  label: string,
+  labelClass?: string,
+  iconClass?: string,
+};
+
+defineProps<EmptyListProps>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-4 items-center" v-tw-merge>
+    <Icon
+      icon="fluent:emoji-sad-slight-20-regular"
+      :class="twMerge('text-primary text-8xl', iconClass)"
+    />
+    <p :class="twMerge('text-xl font-semibold', labelClass)">
+      {{ label }}
+    </p>
+  </div>
+</template>
