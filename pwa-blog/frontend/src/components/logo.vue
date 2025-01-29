83d75bf73e16518dfe08220339a66a23e122d5c3
Diogo Mota <xdimota7x@gmail.com>
2025-01-29 21:31:26 +0000

    Add logo component

---
+++ pwa-blog/frontend/src/components/logo.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Avatar from 'primevue/avatar';
+import { twMerge } from '../utils/tailwind';
+
+type LogoProps = {
+  logoType?: 'avatar-only' | 'full' | 'screen-size',
+  variant?: 'primary' | 'secondary',
+  textClass?: string,
+};
+
+const { variant = 'primary', logoType = 'screen-size' } = defineProps<LogoProps>();
+</script>
+
+<template>
+  <div class="flex flex-row gap-2 items-center" v-tw-merge>
+    <Avatar
+      label="PB"
+      size="normal"
+      shape="circle"
+      :pt:root:class="twMerge('!bg-primary', variant === 'secondary' && '!bg-white')"
+      :pt:label:class="twMerge('font-bold text-white', variant === 'secondary' && 'text-primary')"
+    />
+    <span
+      :class="twMerge(
+        'font-extrabold hidden',
+        logoType === 'screen-size' && 'md:block',
+        logoType === 'full' && 'block',
+        variant === 'secondary' && 'text-white',
+        textClass,
+      )"
+    >
+      PWA Blog
+    </span>
+  </div>
+</template>
