943e7c41129e07c47d77ecd00b4f695ede532229
Diogo Mota <xdimota7x@gmail.com>
2025-02-05 16:51:54 +0000

    Add page-container and page-inner-container components

---
+++ pwa-blog/frontend/src/components/page-container.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PageHeader from './page-header.vue';
+</script>
+
+<template>
+  <div class="flex flex-col min-h-full w-full" v-tw-merge>
+    <PageHeader />
+    <div class="mb-16"></div>
+    <slot></slot>
+  </div>
+</template>
---
+++ pwa-blog/frontend/src/components/page-inner-container.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex flex-col w-full min-h-full max-w-screen-md py-4 px-6 self-center" v-tw-merge>
+    <slot></slot>
+  </div>
+</template>
