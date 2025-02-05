27aeffb08d793e2f4a1129e3048a22666eb79ecb
Diogo Mota <xdimota7x@gmail.com>
2025-02-05 16:50:12 +0000

    Add splash-screen component

---
+++ pwa-blog/frontend/src/components/splash-screen.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Logo from './logo.vue';
+</script>
+
+<template>
+  <div v-tw-merge  class="absolute z-20 top-0 right-0 bottom-0 left-0 bg-primary">
+    <div class="flex h-full w-full items-center justify-center">
+      <Logo logoType="full" variant="secondary" class="animate-pulse" />
+    </div>
+  </div>
+</template>
