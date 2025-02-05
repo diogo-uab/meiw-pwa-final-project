d2bf38354d2b238d29015322a6a01850da2e30d3
Diogo Mota <xdimota7x@gmail.com>
2025-02-05 17:03:54 +0000

    Add not-found page

---
+++ pwa-blog/frontend/src/pages/not-found.page.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+import PageContainer from '../components/page-container.vue';
+import PageInnerContainer from '../components/page-inner-container.vue';
+</script>
+
+<template>
+  <PageContainer>
+    <PageInnerContainer class="flex-1 items-center justify-center">
+      <Icon icon="tabler:error-404" class="text-primary text-9xl" />
+      <h1 class="font-bold text-3xl mb-2">Oops!</h1>
+      <p class="font-bold text-xl">Page not found</p>
+    </PageInnerContainer>
+  </PageContainer>
+</template>
