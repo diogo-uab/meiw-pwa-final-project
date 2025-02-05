bc13acc2f66080f3ed8fe092b158a26b633cd737
Diogo Mota <xdimota7x@gmail.com>
2025-02-05 17:05:23 +0000

    Add auth-container

---
+++ pwa-blog/frontend/src/pages/auth/auth-container.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Logo from '../../components/logo.vue';
+</script>
+
+<template>
+  <div class="flex flex-col h-full w-full bg-gradient-to-r from-primary to-indigo-950">
+    <div class="flex flex-col h-full max-w-screen-md w-full p-4 gap-2 items-center justify-center self-center">
+      <div class="px-12 md:px-24 py-6 rounded-md border shadow-md bg-gray-50">
+        <RouterLink :to="{ name: 'home' }">
+          <Logo class="mb-6" logo-type="full" />
+        </RouterLink>
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
