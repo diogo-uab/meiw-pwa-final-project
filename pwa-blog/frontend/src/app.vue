fa83a2ef630b14b5a27e8e98d0819586771ad0a7
Diogo Mota <xdimota7x@gmail.com>
2025-02-06 00:28:16 +0000

    Add app.vue and index.css

---
+++ pwa-blog/frontend/src/app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ConfirmDialog from 'primevue/confirmdialog';
+import SplashScreen from './components/splash-screen.vue';
+import { useAuthStore } from './stores/auth.store';
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <main class="h-full">
+    <SplashScreen v-if="!authStore.initialAuthGuardDone" />
+    <RouterView />
+    <ConfirmDialog :draggable="false" />
+  </main>
+</template>
