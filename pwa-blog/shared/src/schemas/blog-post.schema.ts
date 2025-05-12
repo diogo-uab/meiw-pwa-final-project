70272b3b3bf02958458a48df665d33b6c173925c
Diogo Mota <xdimota7x@gmail.com>
2025-05-12 21:12:42 +0100

    update shared package blog-post schema to include location property

--- pwa-blog/shared/src/schemas/blog-post.schema.ts
+++ pwa-blog/shared/src/schemas/blog-post.schema.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { UserSchema } from './user.schema';
+import { LocationSchema } from './location.schema';
 import { IdSchema, ReferenceSchema, TimestampSchema } from './utils';
 
 export const BlogPostSchema = z.object({
@@ -8,6 +9,15 @@ export const BlogPostSchema = z.object({
   description: z.string().optional(),
   body: z.string().min(1, 'Body is required'),
   author: ReferenceSchema(UserSchema),
+  location: z.preprocess((data) => {
+    if (
+      data && typeof data === 'object' &&
+      'toObject' in data && typeof data.toObject === 'function'
+    ) {
+      return data.toObject();
+    }
+    return data;
+  }, LocationSchema.nullish().default(null)),
   createdAt: TimestampSchema,
   updatedAt: TimestampSchema.optional(),
 });
